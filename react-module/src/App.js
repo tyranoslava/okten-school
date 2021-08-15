e4d4ff50de1fc3db058d3a580c14e2ab1e522cce
tyranoslava <o.d.lastivka@gmail.com>
2021-08-15 19:56:29 +0300

    init

--- react-module/src/App.js
+++ react-module/src/App.js
@@ -1,13 +1,8 @@
-import './App.css'
-import Product from "./components/product/Product";
-
 export default function App() {
-    return (
-        <div className="products">
-            {/* new Product('milk','20uah'*/}
-            <Product title={'milk'} price={'20uah'}/>
-            <Product title={'juice'} price={'30uah'}/>
-            <Product title={'bread'} price={'15uah'}/>
-        </div>
-    );
+  return (
+    <div>
+    App
+
+    </div>
+  );
 }
